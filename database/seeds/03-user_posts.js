caeaf1ab2014f4f8c926baa6d1b05ea5b5b634c5
Eli Sacks <bastlifa@gmail.com>
2020-07-28 15:22:04 -0400

    user_posts seeds

--- database/seeds/03-user_posts.js
+++ database/seeds/03-user_posts.js
@@ -1,31 +1,31 @@
 exports.seed = function(knex, Promise)
 {
-    return knex('posts')
+    return knex('user_posts')
     .insert(
         [
             {
-                user_id: 0,
-                post_id: 1,
-                vote: 0,
-                creator: true
-            },
-            {
                 user_id: 1,
-                post_id: 0,
-                vote: 1,
-                creator: true
+                post_id: 2,
+                vote: 0,
+                creator: 1
             },
             {
                 user_id: 2,
-                post_id: 2,
+                post_id: 1,
+                vote: 1,
+                creator: 1
+            },
+            {
+                user_id: 3,
+                post_id: 3,
                 vote: 2,
-                creator: true
+                creator: 1
             },
             {
                 user_id: 1,
-                post_id: 2,
+                post_id: 1,
                 vote: 1,
-                creator: false
+                creator: 0
             },
         ]
     )
